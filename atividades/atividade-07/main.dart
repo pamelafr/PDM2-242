81654ad2550429b761fe4bf0832ee2a0cbe380a6
pamelafr <161363626+pamelafr@users.noreply.github.com>
2025-02-04 14:46:25 -0300

    Add files via upload

---
+++ atividades/atividade-07/main.dart
@@ -0,0 +1,20 @@
+// Pamela Frota Araujo
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navegação com Stateful Widgets',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
